304c6d6dd126073c49b9736533f89de303dff7b8
Shamil <maashevshamil@gmail.com>
2025-12-27 17:17:45 +0300

    first commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-responsive class="border rounded" max-height="300">
+    <v-app>
+      <v-app-bar title="App bar"></v-app-bar>
+
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-app>
+  </v-responsive>
+</template>
